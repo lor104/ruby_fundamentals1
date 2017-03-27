b60823d41fff9ceb9047748a24d5ec95e223dae0
lor104 <lauren.rosentzveig@gmail.com>
2017-03-27 14:03:14 -0400

    finish workign with exercise3.rb

--- exercise3.rb
+++ exercise3.rb
@@ -2,3 +2,11 @@ puts "what is your name?"
 
 name = gets.chomp
 puts "Hi #{name}"
+
+
+# how old someone is
+
+puts "How old are you?"
+age = gets.chomp
+birthYear = 2017-age.to_i
+puts "#{name}, you were born in #{birthYear}"
