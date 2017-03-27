94a33a8405189d64bbdd0bb41eaf76609f6b6c0f
lor104 <lauren.rosentzveig@gmail.com>
2017-03-27 13:57:21 -0400

    finish working with variable_and_assignment

---
+++ variables_and_assignment.rb
@@ -0,0 +1,7 @@
+my_variable = "my_variable is now this string"
+
+name = "Lauren"
+greeting = "Hello #{name}! It's good to see you again. "
+mission = "Your mission, should you choose to accept it..."
+
+puts greeting + mission
